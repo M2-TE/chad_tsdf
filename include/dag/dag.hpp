9d4cea519e73e9187e9f9f1f66bffc668aeb87d0
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-09-02 15:31:03 +0200

    fixed problematic interface

--- include/dag/dag.hpp
+++ include/dag/dag.hpp
@@ -8,13 +8,6 @@ public:
     DAG();
     // helper function to insert points into DAG
     template<typename PosArr, typename RotArr>
-    void insert(std::span<PosArr> points, PosArr position, RotArr rotation) {
-        std::array<float, 3>* pos_p = reinterpret_cast<std::array<float, 3>*>(&position);
-        std::array<float, 4>* rot_p = reinterpret_cast<std::array<float, 4>*>(&rotation);
-        insert(reinterpret_cast<std::array<float, 3>*>(points.data()), points.size(), *pos_p, *rot_p);
-    }
-    // helper function to insert points into DAG
-    template<typename PosArr, typename RotArr>
     void insert(std::vector<PosArr>& points, PosArr position, RotArr rotation) {
         std::array<float, 3>* pos_p = reinterpret_cast<std::array<float, 3>*>(&position);
         std::array<float, 4>* rot_p = reinterpret_cast<std::array<float, 4>*>(&rotation);
