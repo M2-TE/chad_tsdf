d66ead066f940021dc840dfdc323da484261d501
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-09-03 12:53:18 +0200

    exposing node as separate header

--- src/dag/dag.cpp
+++ src/dag/dag.cpp
@@ -4,6 +4,7 @@
 #include "dag/levels.hpp"
 #include "dag/morton.hpp"
 #include "dag/octree.hpp"
+#include "dag/node.hpp"
 
 auto insert_octree(Octree& octree, std::vector<glm::vec3>& points, std::vector<glm::vec3>& normals, 
     std::array<NodeLevel, 20>& node_levels, LeafLevel& leaf_level) -> uint32_t
@@ -154,60 +155,6 @@ auto insert_octree(Octree& octree, std::vector<glm::vec3>& points, std::vector<g
     fmt::println("dag  ctor {:.2f}", dur);
     return root_addr;
 }
-// pure helper struct for managing DAG nodes, will never be explicitly created or destroyed
-struct Node {
-    Node() = delete;
-    ~Node() = delete;
-    Node(const Node&) = delete;
-    Node(Node&&) = delete;
-    Node& operator=(const Node&) = delete;
-    Node& operator=(Node&&) = delete;
-
-    // reinterpret a node address as a valid Node object
-    auto static inline from_addr(std::vector<uint32_t>& data, uint_fast32_t addr) -> Node* {
-        return reinterpret_cast<Node*>(&data[addr]);
-    }
-    // check header mask for a specific child
-    bool inline contains_child(uint32_t child_i) const {
-        uint32_t child_bit = 1 << child_i;
-        return _header & child_bit;
-    }
-    // retrieve child addr from sparse children array
-    auto inline get_child_addr(uint32_t child_i) const -> uint32_t {
-        // count how many children come before this one
-        uint32_t child_bit = 1 << child_i;
-        uint32_t masked = _header & (child_bit - 1);
-        uint32_t child_count;
-#       ifdef CHAD_POPCOUNT_INSTRUCTION
-            child_count = CHAD_POPCOUNT_INSTRUCTION(masked);
-#       else
-            // popcount lookup table: https://stackoverflow.com/a/51388543
-            constexpr uint8_t popcount_table[] = {
-                0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
-                1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-                1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-                2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-                2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-                1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-                2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-                2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-                3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-                4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
-            };
-            child_count = popcount_table[masked];
-#       endif
-        return _children[child_count];
-    }
-
-    uint32_t _header;
-    std::array<uint32_t, 8> _children; // at least 1 valid child, at most 8
-};
 // merge dag subtree obtained from scan into primary subtree
 void merge_primary(uint_fast32_t root_addr, std::array<NodeLevel, 20>& node_levels, LeafLevel& leaf_level) {
     auto beg = std::chrono::steady_clock::now();
