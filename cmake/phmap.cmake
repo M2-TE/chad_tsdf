9da4245f92d90802c1dce672dd4748461d9bb968
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-09-05 10:52:15 +0200

    rev

--- cmake/phmap.cmake
+++ cmake/phmap.cmake
@@ -1,3 +1,3 @@
-FetchContent_Declare(parallel-hashmap GIT_REPOSITORY "https://github.com/greg7mdp/parallel-hashmap.git" GIT_TAG "v1.3.12" GIT_SHALLOW ON)
-FetchContent_MakeAvailable(parallel-hashmap)
-target_link_libraries(${PROJECT_NAME} PRIVATE parallel-hashmap)
+FetchContent_Declare(phmap GIT_REPOSITORY "https://github.com/greg7mdp/parallel-hashmap.git" GIT_TAG "v1.3.12" GIT_SHALLOW ON)
+FetchContent_MakeAvailable(phmap)
+target_link_libraries(${PROJECT_NAME} PRIVATE phmap)
