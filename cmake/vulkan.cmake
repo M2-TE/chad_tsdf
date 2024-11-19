1f7c8f1e2c39f1a693c012e665c39058c67a1f13
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-11-19 14:10:27 +0100

    updated for ros conformity

--- cmake/vulkan.cmake
+++ cmake/vulkan.cmake
@@ -3,15 +3,13 @@ FetchContent_Declare(vulkan-headers
     GIT_TAG "v1.3.290"
     GIT_SHALLOW ON
     GIT_SUBMODULES ""
-    SOURCE_SUBDIR "disabled"
-    OVERRIDE_FIND_PACKAGE)
+    SOURCE_SUBDIR "disabled")
 FetchContent_Declare(vulkan-hpp
     GIT_REPOSITORY "https://github.com/KhronosGroup/Vulkan-Hpp.git"
     GIT_TAG "v1.3.290"
     GIT_SHALLOW ON
     GIT_SUBMODULES ""
-    SOURCE_SUBDIR "disabled"
-    OVERRIDE_FIND_PACKAGE)
+    SOURCE_SUBDIR "disabled")
 FetchContent_MakeAvailable(vulkan-headers vulkan-hpp)
 target_include_directories(${PROJECT_NAME} SYSTEM PRIVATE
     "${vulkan-headers_SOURCE_DIR}/include"
