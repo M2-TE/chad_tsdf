9e2496b5697d4441f30b169c9db5ca5986495420
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-12-03 13:45:32 +0100

    added some documentation

--- include/dag/dag.hpp
+++ include/dag/dag.hpp
@@ -9,32 +9,64 @@
 
 struct DAG {
     DAG();
-    // helper function to insert points into DAG
+
+    /**
+     * @brief Main function to insert new points
+     * 
+     * @param points_p array of 3D points
+     * @param points_count number of points
+     * @param position scanner position
+     * @param rotation scanner rotation
+     */
+    void insert(std::array<float, 3>* points_p, std::size_t points_count, std::array<float, 3> position, std::array<float, 4> rotation);
+    /**
+     * @brief Main function to insert new points
+     * 
+     * @param points_p array of 3D points
+     * @param points_count number of points
+     * @param position scanner position
+     * @param rotation scanner rotation
+     */
     template<typename PosArr, typename RotArr>
     void insert(std::vector<PosArr>& points, PosArr position, RotArr rotation) {
         std::array<float, 3>* pos_p = reinterpret_cast<std::array<float, 3>*>(&position);
         std::array<float, 4>* rot_p = reinterpret_cast<std::array<float, 4>*>(&rotation);
         insert(reinterpret_cast<std::array<float, 3>*>(points.data()), points.size(), *pos_p, *rot_p);
     }
-    // main function to insert points into DAG'
-
+    /**
+     * @brief Merge target subtree into the global map
+     * 
+     * @param root_addr root address of the target subtree
+     */
+    void merge_subtree(uint_fast32_t root_addr);
 
     /**
-     * @brief 
-     * 
-     * @param points_p 
-     * @param points_count 
-     * @param position 
-     * @param rotation 
+     * @brief Merge all subtrees into the global map
      */
-    void insert(std::array<float, 3>* points_p, std::size_t points_count, std::array<float, 3> position, std::array<float, 4> rotation);
-    // TODO: insert via 4x4 mat as pose
-    // TODO: rotation as euler angles
-    void merge_subtree(uint_fast32_t root_addr);
     void merge_all_subtrees();
+
+    /**
+     * @brief Print statistics into the console
+     */
     void print_stats();
+
+    /**
+     * @brief Get the memory footprint of the node vectors
+     * 
+     * @return memory footprint in bytes
+     */
     auto get_readonly_size() -> double;
+
+    /**
+     * @brief Get the memory footprint of the hash structures
+     * 
+     * @return memory footprint in bytes
+     */
     auto get_hash_size() -> double;
+
+    /**
+     * @brief Ignore this one
+     */
     auto debug_iterate_all_leaves_of_subtree(uint32_t root_addr) -> std::size_t;
 
     // get raw data from normal dag levels
