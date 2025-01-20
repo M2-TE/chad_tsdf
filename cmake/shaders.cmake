8cf7b55e48922116b65ae4624f65767dd20f423a
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2025-01-20 15:04:29 +0100

    set specific spvrc version

--- cmake/shaders.cmake
+++ cmake/shaders.cmake
@@ -16,8 +16,8 @@ target_link_libraries(${PROJECT_NAME} PRIVATE spirv-reflect-static)
 set(SPVRC_SHADER_DIR "${CMAKE_CURRENT_SOURCE_DIR}/shaders")
 set(SPVRC_SHADER_ENV "vulkan1.1")
 FetchContent_Declare(spvrc
-    GIT_REPOSITORY "https://github.com/M2-TE/spvrc.git"
-    GIT_TAG "main"
+    GIT_REPOSITORY "https://github.com/M2-TE/SPIRV-RC.git"
+    GIT_TAG "v1.0.2"
     GIT_SHALLOW ON)
 FetchContent_MakeAvailable(spvrc)
 target_link_libraries(${PROJECT_NAME} PRIVATE spvrc::spvrc)
